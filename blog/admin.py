ae13960ac2452b6bb6f46cf168ee208f63dbf3ef
DESKTOP-PK9B0IA\Hyuk <rlagur9286@naver.com>
2017-08-25 00:19:00 +0900

    blog.models.Post 모델을 등록하고, admin 연결

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created_at', 'is_public']
+    list_display_links = ['title']
+    list_editable = ['is_public']
