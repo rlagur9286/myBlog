ee874dcc4996b6242795f953d13f4bbb9a8c15c7
DESKTOP-PK9B0IA\Hyuk <rlagur9286@naver.com>
2017-08-29 17:30:32 +0900

    2017-08-29, H, post_list.html 추가

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def index(request):
+    post_list = Post.objects.all()
+    return render(request, 'blog/post_list.html',{
+        'post_list': post_list
+    })
+
+
+def post_list(request):
+    post_list = Post.objects.all()
+    return render(request, 'blog/post_list.html', {
+        'post_list': post_list
+    })
