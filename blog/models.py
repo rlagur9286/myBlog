c52329e39123f79e1466b10ab5f847ef5d976872
DESKTOP-PK9B0IA\Hyuk <rlagur9286@naver.com>
2017-08-31 19:05:06 +0900

    2017-08-31, H, google 위젯에 lat, lng 기본 값 추가, validator 추가

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,32 @@
 from django.db import models
+import re
+from django import forms
+
+
+def lnglat_validator(value):
+    if not re.match(r'[+-]?\d+\.?\d*, [+-]?\d+\.?\d*', value):
+        raise forms.ValidationError('경도/위도를 입력 해주세요.')
 
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    photo = models.ImageField()
+    photo = models.ImageField(blank=True)
     is_public = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    lnglat = models.CharField(max_length=50, blank=True, help_text="경도/위도 입력")
+    lnglat = models.CharField(max_length=50, blank=True, help_text="지도에서 선택 해주세요.", validators=[lnglat_validator])
 
     def __str__(self):
         return self.title
+
+    def lng(self):
+        if self.lnglat:
+            return self.lnglat.split(',')[0]
+        return None
+
+    def lat(self):
+        if self.lnglat:
+            return self.lnglat.split(',')[1]
+        return None
+
