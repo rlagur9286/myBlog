47f65f3f63de847bd6114131eea534b26a47d1df
DESKTOP-PK9B0IA\Hyuk <rlagur9286@naver.com>
2017-08-31 21:40:33 +0900

    2017-08-31, H, login, signup 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.conf import settings
 
-# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+            return redirect(settings.LOGIN_URL)
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup_form.html', {
+        'form': form,
+    })
