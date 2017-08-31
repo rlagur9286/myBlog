c201f7782842bbff8ab52eb67c5c95203d3a2246
DESKTOP-PK9B0IA\Hyuk <rlagur9286@naver.com>
2017-08-31 21:16:24 +0900

    2017-08-31, H, accounts 추가

--- photowall/urls.py
+++ photowall/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^blog/', include('blog.urls', namespace='blog')),
+    url(r'^accounts/', include('accounts.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
